0299dc7ae074899fd8b4568b145053c3a031caf7
Vitalii Dudnik <dudnikvtl@gmail.com>
2023-04-15 13:59:45 +0300

    v0.5

--- js/calculator.js
+++ js/calculator.js
@@ -17,14 +17,38 @@ class Calculator {
 
   inputNumber(number) {
     this.screenElement.value += number;
+
+    // const currentInput = this.screenElement.value;
+    // const lastCharacter = currentInput[currentInput.length - 1];
+
+    // if (lastCharacter === "." && number === ".") {
+    //   // If the last character is already a decimal point and the user tries to input another decimal point
+    //   return; // Do not append the additional decimal point
+    // }
+
+    // if (currentInput === "" && number !== ".") {
+    //   // If the current input is "0" and the user tries to input a digit other than a decimal point
+    //   this.screenElement.value = number; // Replace the current input with the new digit
+    // } else if (currentInput === "" && number === ".") {
+    //   // If the current input is empty and the user tries to input a decimal point
+    //   return; // Set the input as "0." to start a decimal number
+    // }
+    // else if (!/^\d+(\.\d*)?$/.test(currentInput + number)) {
+    //   // If the current input and the new number do not form a valid decimal number
+    //   return; // Do not append the new number
+    // } else {
+    //   this.screenElement.value += number;
+    // }
   }
 
   inputOperation(operationName) {
-    let operation = null;
-    let operators = ["+", "-", "/", "x"];
+    // let operation = null;
+    let operators = ["+", "-", "/", "*", "."];
 
-    const isOpsNotIncludes = (ops) => {
-      let lastSymbol = this.screenElement.value.slice(this.screenElement.value.length - 1);
+    const isOpsNotIncluded = (ops) => {
+      let lastSymbol = this.screenElement.value.slice(
+        this.screenElement.value.length - 1
+      );
       if (!ops.includes(lastSymbol)) {
         return true;
       } else {
@@ -34,19 +58,19 @@ class Calculator {
 
     if (operationName === "add") {
       if (this.screenElement.value !== "") {
-        if (isOpsNotIncludes(operators)) {
-          operation = "+";
+        if (isOpsNotIncluded(operators)) {
+          // operation = "+";
           // this.screenElement.value += "+";
           // split + join instead of "="
-          this.screenElement.value.slice(this.screenElement.value.length - 1) = "+";
+          this.screenElement.value += "+";
         }
       }
     }
 
     if (operationName === "subtract") {
       if (this.screenElement.value !== "") {
-        if (isOpsNotIncludes(operators)) {
-          operation = "-";
+        if (isOpsNotIncluded(operators)) {
+          // operation = "-";
           this.screenElement.value += "-";
         }
       }
@@ -54,8 +78,8 @@ class Calculator {
 
     if (operationName === "divide") {
       if (this.screenElement.value !== "") {
-        if (isOpsNotIncludes(operators)) {
-          operation = "/";
+        if (isOpsNotIncluded(operators)) {
+          // operation = "/";
           this.screenElement.value += "/";
         }
       }
@@ -63,11 +87,42 @@ class Calculator {
 
     if (operationName === "multiply") {
       if (this.screenElement.value !== "") {
-        if (isOpsNotIncludes(operators)) {
-          operation = "*";
-          this.screenElement.value += "x";
+        if (isOpsNotIncluded(operators)) {
+          // operation = "*";
+          this.screenElement.value += "*";
         }
       }
+    }
+
+    if (operationName === ".") {
+      if (this.screenElement.value !== "") {
+        if (isOpsNotIncluded(operators)) {
+          // operation = ".";
+          this.screenElement.value += ".";
+        }
+      }
+    }
+
+    if (operationName === "equals") {
+      let currentResult = this.screenElement.value;
+      try {
+        let answer = math.evaluate(currentResult);
+        this.screenElement.value = answer;
+      } catch (error) {
+        currentResult = "Error";
+      }
+    }
+
+    if (operationName === "del") {
+      let currentResult = this.screenElement.value;
+      this.screenElement.value = currentResult.substring(
+        0,
+        currentResult.length - 1
+      );
+    }
+
+    if (operationName === "reset") {
+      this.screenElement.value = "";
     }
   }
 }
